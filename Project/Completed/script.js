e2dd415693f9f202f46d11b24d5ba11fd4c3191e
adnan-sabbir <adnan.sabbir@g.bracu.ac.bd>
2021-07-18 04:44:10 +0600

    Initial commit

--- Project/Completed/script.js
+++ Project/Completed/script.js
@@ -1,22 +1,64 @@
-function updateDisplayText(value){
-	const displayTextElem = document.getElementById('display-text');
-	if(!value){
-		displayTextElem.innerText = 'Please enter any text';
-		displayTextElem.setAttribute('class', 'empty-text');
-	}else{
-		displayTextElem.innerText = value;
-		displayTextElem.setAttribute('class', 'has-text');
+const registerForm = document.getElementById('register-form');
+registerForm.addEventListener('submit', validateForm);
+const registerFormErrorField = registerForm.getElementsByClassName('error-message')[0];
+
+function validateForm(){
+	const inputElements = registerForm.getElementsByTagName('input');
+	let valid = true;
+	const data = {};
+
+	for(let i = 0; i<inputElements.length; i++){
+		const {name, value} = inputElements[i];
+		const trimmedValue = value.trim();
+		data[name] = value;
+
+		if(!trimmedValue){
+			valid = false;
+		}
 	}
+
+	if(!valid) return;
+
+	registerUser(data)
+		.then(response=> {
+			login(data.username, data.password);
+		})
+		.then(response=> {
+			window.location = 'file:///C:/Users/adnan/OneDrive/Desktop/getting_started_with_frontend/Project/Completed/dashboard.html';
+		})
+		.catch(response=> {
+			registerFormErrorField.innerText = response.message;
+		});
 }
 
-function clearInputBox(){
-	const inputBox = document.getElementById("input-field");
-	inputBox.value = '';
+function registerUser(newUserData){
+	return new Promise((resolve, reject)=> {
+		let userData = JSON.parse(localStorage.getItem('userData'));
+		if(!userData)userData = [];
 
-	updateDisplayText(null);
+		for(let user of userData){
+			if(user.username === newUserData.username) reject({success: false, message: 'username already exist'});
+			if(user.email === newUserData.email) reject({success: false, message: 'email already exist'});
+			return;
+		}
+
+		userData.push(newUserData);
+		localStorage.setItem('userData', JSON.stringify(userData));
+		resolve({success: true});
+	});
 }
 
-function onLoad(){
-	const inputBox = document.getElementById("input-field");
-	updateDisplayText(inputBox.value);
+function login(username, pass){
+	const userData = JSON.parse(localStorage.getItem('userData'));
+	return new Promise((resolve, reject)=> {
+		if(!userData) reject({success: false, message: 'wrong credentials'});
+
+		const user = userData.find(user => user.username === username);
+		if(user.password === pass){
+			localStorage.setItem('loggedInUser', JSON.stringify(user));
+			resolve({success: true});
+		}
+		else reject({success: false, message: 'wrong credentials'});
+	});
+	
 }
